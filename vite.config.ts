d922f8f84c144301786e4522d0baa22c881a072f
rkdemontfort-collab <rkdemontfort@gmail.com>
2026-02-14 17:27:37 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: "./", 
+  base: "/TRUST-APP/", 
   server: {
     host: "::",
     port: 8080,
